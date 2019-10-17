064e07203733eba44cc6f77a1ef46228771d0ec9
Benjamin Sunderland <ben@sunderland.eu.com>
2019-10-17 16:28:54 +0200

    added css and bodyparser

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 const express = require('express')
 const app = express()
 
+const bodyParser = require('body-parser');
+
 app.set('view engine', 'ejs')
 
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get('/', function (req, res) {
-  res.send('index')
+  res.render('index')
 })
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
+})
+
+app.post('/', (req, res, next) => {
+
 })
